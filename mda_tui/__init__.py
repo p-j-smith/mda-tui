b69e16038b129f83dc7a5dda3bee39b234da4fb1
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-09-02 17:59:40 +0100

    Initial creation with MDAnalysis Cookiecutter 0.1

---
+++ mda_tui/__init__.py
@@ -0,0 +1,13 @@
+"""
+MDAnalysis TUI
+A textual user interface (TUI) for MDAnalysis
+"""
+
+# Add imports here
+
+# Handle versioneer
+from ._version import get_versions
+versions = get_versions()
+__version__ = versions['version']
+__git_revision__ = versions['full-revisionid']
+del get_versions, versions
