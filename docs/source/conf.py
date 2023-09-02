f0b29739368de7590f126d1d2b8fdc0f0eefde8b
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-09-02 21:30:00 +0100

    Update linting / formatting config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 # Incase the project was not installed
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.././src'))
+sys.path.insert(0, os.path.abspath('../../src'))
 import mda_tui  # noqa
 
 
