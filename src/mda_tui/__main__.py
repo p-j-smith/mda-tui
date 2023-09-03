83794e9a7712b15360cd50ba51e3f86c72e505a1
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-09-03 19:10:18 +0100

    Add header, footer, title, ans sub-title

--- src/mda_tui/__main__.py
+++ src/mda_tui/__main__.py
@@ -1,8 +1,15 @@
-from textual.app import App
+from textual import app, widgets
 
 
-class MDA(App):
-    pass
+class MDA(app.App[str]):
+    def __init__(self):
+        super().__init__()
+        self.title = "MDA TUI"
+        self.sub_title = "On-the-fly transformations and trajectory analysis in your terminal!"
+
+    def compose(self) -> app.ComposeResult:
+        yield widgets.Header()
+        yield widgets.Footer()
 
 
 def main():
