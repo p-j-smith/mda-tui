97feb72a254be1547feb55fcfff8bf2a3c5caa69
Paul Smith <paul.j.smith@ucl.ac.uk>
2023-09-11 11:41:36 +0100

    use setuptools-scm to manage version

--- src/mda_tui/__init__.py
+++ src/mda_tui/__init__.py
@@ -3,12 +3,4 @@ MDAnalysis TUI
 A textual user interface (TUI) for MDAnalysis
 """
 
-# Add imports here
-
-# Handle versioneer
-from ._version import get_versions
-
-versions = get_versions()
-__version__ = versions["version"]
-__git_revision__ = versions["full-revisionid"]
-del get_versions, versions
+from ._version import __version__, __version_tuple__
